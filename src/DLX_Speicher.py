3b1472db21619af24f07486e17e1c122f85169bd
botcor <cornelius.bott@gmx.de>
2014-12-04 16:34:14 +0100

    Create DLX_Speicher.py

---
+++ src/DLX_Speicher.py
@@ -0,0 +1,44 @@
+import logging
+from bitstring import *
+
+class DLX_Speicher:
+    storage = []
+    size = 2048
+    def __init__(self, size=2048):
+        x =  BitArray(uint=0, length=8)
+        for i in range(0, size):
+            self.storage.append(x)
+    
+    def setB(self, adr, value):
+        self.storage[adr] = value
+
+    def setHW(self, adr, value):
+        if adr % 2 == 0:
+            self.storage[adr] = value[0:8]
+            self.storage[adr +1] = value[8:16]
+
+    def setW(self, adr, value):
+        if adr % 4 == 0:
+            self.storage[adr] = value[0:8]
+            self.storage[adr +1] = value[8:16]
+            self.storage[adr +2] = value[16:24]
+            self.storage[adr +3] = value[24:32]
+
+    def getB(self, adr):
+        return self.storage[adr]
+
+    def getHW(self,adr):
+        if adr % 2 == 0:
+            x = BitArray(uint=0, length=16)
+            x[0:8] = self.storage[adr]
+            x[8:16] = self.storage[adr +1]
+            return x
+
+    def getW(self, adr):
+        if adr % 4 == 0:
+            x = BitArray(uint=0, length=32)
+            x[0:8] = self.storage[adr]
+            x[8:16] = self.storage[adr +1]
+            x[16:24] = self.storage[adr +2]
+            x[24:32] = self.storage[adr +3]
+            return x
