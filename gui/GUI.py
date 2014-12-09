5438fe556e038c9dc2b6cd1a7805edb632141464
Cornelius Bott <botcor@hs-pforzheim.de>
2014-12-09 14:36:41 +0100

    add /gui and  mainwindow.ui

---
+++ gui/GUI.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python     
+
+# Import PySide classes
+import sys
+from PySide.QtCore import *
+from PySide.QtGui import *
+from MainWindow import *
+     
+     
+class ControlMainWindow(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        super(ControlMainWindow, self).__init__(parent)
+        self.ui =  Ui_MainWindow()
+        self.ui.setupUi(self)
+   
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    mySW = ControlMainWindow()
+    mySW.show()
+    sys.exit(app.exec_())
