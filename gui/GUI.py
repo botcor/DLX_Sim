5cd31051a4ac33ebd4b52fe75bcc86cb27e189a2
Cornelius Bott <botcor@hs-pforzheim.de>
2015-02-16 14:04:24 +0100

    changes on the dialogs

--- gui/GUI.py
+++ gui/GUI.py
@@ -25,12 +25,13 @@ def setOpenFileName():
     open_at = "/home/"
     fileName = QtGui.QFileDialog.getOpenFileName(parent, title, open_at, filters)
     mySIM.collectData(fileName[0])
+    memmod.setContent(mySIM.storage)
     return fileName
 
 def Reset():
     # construct and connect the models to the views
     progmod = ProgramModel(mySW.ui.programview)
-    pipemod = PipelineModel(mySW.ui.pipeview, myStubSIM)
+    pipemod = PipelineModel(mySW.ui.pipeview, mySIM)
     regmod = RegisterModel(mySW.ui.registerview)
     memmod = MemoryModel(mySW.ui.memoryview)
     # setup the initial content of the models
@@ -40,11 +41,13 @@ def Reset():
 def goNext():
     mySIM.doPipe(1)
     pipemod.updateContent()
+    regmod.updateContent()
 
 def goMore():
     num_steps = QtGui.QInputDialog.getInt( mySW, "Prompt", "Please insert a Number:", 2)
     mySIM.doPipe(num_steps)
     pipemod.updateContent()
+    regmod.updateContent()
 
 def Run():
     #myStubSIM.NextStep(True)
@@ -53,34 +56,26 @@ def Run():
 def quitApp():
     app.quit()
 
-def showProgram(checked):
-    if(checked):    
-        progmod.setContent()
-
-def showRegisters(checked):
-    if(checked):
-        regmod.setContentInitial(mySIM)
-
-def showMemory(checked):
-    if(checked):
-        memmod.setContent(mySIM.storage)
-
-def showPipeline(checked):
-    if(checked):
-        pipemod.updateContent()
-
 def changeMemSize():
-    new_size = QtGui.QInputDialog.getInt( mySW, "Prompt", "Please insert the new size:", mySIM.pipe.storage.size)
+    new_size = QtGui.QInputDialog.getInt( mySW, "Prompt", "Please insert the new memory size:", mySIM.pipe.storage.size)
     #mySIM.pipeline.storage.size = new_size
 
 def switchFWD(checked):
-    dialog = QtGui.QMessageBox(QtGui.QMessageBox.Information, "Question", "To Switch on/off the Forwarding the DLX has to be reset. Do you really want to do so?", QtGui.QMessageBox.Ok|QtGui.QMessageBox.Cancel, mySW, 0)
+    dialog = QtGui.QMessageBox(QtGui.QMessageBox.Information, "Info", "The DLX will be reset", QtGui.QMessageBox.Ok, mySW, 0)
     answer = dialog.exec()
     print(answer)
+    if(checked == True):
+        mySIM.pipe.fForwarding = True
+    else:
+        mySIM.pipe.fForwarding = False
+    print(mySIM.pipe.fForwarding)
 
 def Reset():
     dialog = QtGui.QMessageBox(QtGui.QMessageBox.Information, "Question", "Do you really want to reset the DLX?", QtGui.QMessageBox.Ok|QtGui.QMessageBox.Cancel, mySW, 0)
     answer = dialog.exec()
+    print(answer)
+    if(answer == 1024):
+        mySIM.pipe.ResetPipeline()
 
 if __name__ == "__main__":
     # general stuff
@@ -91,22 +86,18 @@ if __name__ == "__main__":
     myStubSIM = StubSim()
     # construct and connect the models to the views
     progmod = ProgramModel(mySW.ui.programview)
-    pipemod = PipelineModel(mySW.ui.pipeview, myStubSIM)
+    pipemod = PipelineModel(mySW.ui.pipeview, mySIM)
     regmod = RegisterModel(mySW.ui.registerview)
     memmod = MemoryModel(mySW.ui.memoryview)
     # setup the initial content of the models
     pipemod.setContentInitial()
+    regmod.setContentInitial(mySIM)
     # connect the menu actions to the custom functions
     mySW.ui.action_LoadProgram.triggered.connect(setOpenFileName)
     mySW.ui.action_ResetDLX.triggered.connect(Reset)
     mySW.ui.action_NextStep.triggered.connect(goNext)
     mySW.ui.action_MoreSteps.triggered.connect(goMore)
     mySW.ui.action_Run.triggered.connect(Run)
-    mySW.ui.action_ProgramView.toggled.connect(showProgram)
-    mySW.ui.action_RegisterView.toggled.connect(showRegisters)
-    mySW.ui.action_MemoryView.toggled.connect(showMemory)
-    mySW.ui.action_PipelineView.toggled.connect(showPipeline)
-    mySW.ui.action_ProgramView.setChecked(True)
     mySW.ui.action_Forwarding.toggled.connect(switchFWD)
     mySW.ui.action_MemorySize.triggered.connect(changeMemSize)
     sys.exit(app.exec_())
--- gui/Models.py
+++ gui/Models.py
@@ -66,89 +66,41 @@ class PipelineModel(Model):
         self.model.addLine(CurserX + self.Colm_Width, 0, CurserX + self.Colm_Width, 400, self.Pen1)
         # add the rectangles
 
-        if(pSim.zustand[1] != 0):
-            idx = pSim.CommandStatus.index('IF')
+        if(pSim.zustand[0] != 0):
+            idx = pSim.zustand[0]
             CurserY = 30 + self.Line_Height * idx
             self.model.addRect(CurserX, CurserY, self.Colm_Width, self.Line_Height, self.Pen, self.Brush1)
             label = self.model.addText("IF")
             label.setX(CurserX)
             label.setY(CurserY)
-        if(pSim.CommandStatus.count('ID') == 1):
-            idx = pSim.CommandStatus.index('ID')
+        if(pSim.zustand[1] != 0):
+            idx = pSim.zustand[1]
             CurserY = 30 + self.Line_Height * idx
             self.model.addRect(CurserX, CurserY, self.Colm_Width, self.Line_Height, self.Pen, self.Brush2)
             label = self.model.addText("IF")
             label.setX(CurserX)
             label.setY(CurserY)
-        if(pSim.CommandStatus.count('EX') == 1):
-            idx = pSim.CommandStatus.index('EX')
+        if(pSim.zustand[2] != 0):
+            idx = pSim.zustand[2]
             CurserY = 30 + self.Line_Height * idx
             self.model.addRect(CurserX, CurserY, self.Colm_Width, self.Line_Height, self.Pen, self.Brush3)
             label = self.model.addText("IF")
             label.setX(CurserX)
             label.setY(CurserY)
-        if(pSim.CommandStatus.count('MEM') == 1):
-            idx = pSim.CommandStatus.index('MEM')
+        if(pSim.zustand[3] != 0):
+            idx = pSim.zustand[3]
             CurserY = 30 + self.Line_Height * idx
             self.model.addRect(CurserX, CurserY, self.Colm_Width, self.Line_Height, self.Pen, self.Brush4)
             label = self.model.addText("IF")
             label.setX(CurserX)
             label.setY(CurserY)
-        if(pSim.CommandStatus.count('WB') == 1):
-            idx = pSim.CommandStatus.index('WB')
+        if(pSim.zustand[4] != 0):
+            idx = pSim.zustand[4]
             CurserY = 30 + self.Line_Height * idx
             self.model.addRect(CurserX, CurserY, self.Colm_Width, self.Line_Height, self.Pen, self.Brush5)
             label = self.model.addText("IF")
             label.setX(CurserX)
             label.setY(CurserY)
-
-#        if not self.pSim.isStall:
-#            if self.pSim.CommandIndex > 4:
-#                self.model.addRect(CurserX, CurserY - 4 * self.Line_Height, self.Colm_Width, self.Line_Height, self.Pen, self.Brush5)
-#                label = self.model.addText("WB")
-#                label.setX(CurserX)
-#                label.setY(CurserY - 4 * self.Line_Height)
-#            if self.pSim.CommandIndex > 3:
-#                self.model.addRect(CurserX, CurserY - 3 * self.Line_Height, self.Colm_Width, self.Line_Height, self.Pen, self.Brush4)
-#                label = self.model.addText("MEM")
-#                label.setX(CurserX)
-#                label.setY(CurserY - 3 * self.Line_Height)
-#            if self.pSim.CommandIndex > 2:
-#                self.model.addRect(CurserX, CurserY - 2 * self.Line_Height, self.Colm_Width, self.Line_Height, self.Pen, self.Brush3)
-#                label = self.model.addText("EX")
-#                label.setX(CurserX)
-#                label.setY(CurserY - 2 * self.Line_Height)
-#            if self.pSim.CommandIndex > 1:
-#                self.model.addRect(CurserX, CurserY - 1 * self.Line_Height, self.Colm_Width, self.Line_Height, self.Pen, self.Brush2)
-#                label = self.model.addText("ID")
-#                label.setX(CurserX)
-#                label.setY(CurserY - 1 * self.Line_Height)
-#            if self.pSim.CommandIndex > 0:
-#                self.model.addRect(CurserX, CurserY - 0 * self.Line_Height, self.Colm_Width, self.Line_Height, self.Pen, self.Brush1)
-#                label = self.model.addText("IF")
-#                label.setX(CurserX)
-#                label.setY(CurserY - 0 * self.Line_Height)
-#        else:
-#            if self.pSim.CommandIndex > 3:
-#                self.model.addRect(CurserX, CurserY - 3 * self.Line_Height, self.Colm_Width, self.Line_Height, self.Pen, self.Brush5)
-#                label = self.model.addText("WB")
-#                label.setX(CurserX)
-#                label.setY(CurserY - 3 * self.Line_Height)
-#            if self.pSim.CommandIndex > 2:
-#                self.model.addRect(CurserX, CurserY - 2 * self.Line_Height, self.Colm_Width, self.Line_Height, self.Pen, self.Brush4)
-#                label = self.model.addText("MEM")
-#                label.setX(CurserX)
-#                label.setY(CurserY - 2 * self.Line_Height)
-#            if self.pSim.CommandIndex > 1:
-#                self.model.addRect(CurserX, CurserY, self.Colm_Width, self.Line_Height, self.Pen, self.Brush2)
-#                label = self.model.addText("Stall")
-#                label.setX(CurserX)
-#                label.setY(CurserY - 1 * self.Line_Height)
-#            if self.pSim.CommandIndex > 0:
-#                self.model.addRect(CurserX, CurserY, self.Colm_Width, self.Line_Height, self.Pen, self.Brush1)
-#                label = self.model.addText("Stall")
-#                label.setX(CurserX)
-#                label.setY(CurserY - 0 * self.Line_Height)
 
 class MemoryModel(Model):
     def setContent(self,storage):
@@ -178,7 +130,7 @@ class RegisterModel(Model):
         # Add the items to the model
         for i in range(0, len(self.item)-1):
             self.model.appendRow(self.item[i])
-    def setContent(self,sim):
+    def updateContent(self,sim):
         # Add the items to the model
         for i in range(1, self.item.length-1):
             self.model.setItem(i, self.item[i])
